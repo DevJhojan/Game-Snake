d4b958f21763850b566a6f6d0178dc3de0c91309
jdtp125753 <jhojan1806@gmail.com>
2023-04-06 17:02:43 -0500

    add game complete

---
+++ Snake/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnStar = new System.Windows.Forms.Button();
+            this.lbPoints = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnStar
+            // 
+            this.btnStar.ForeColor = System.Drawing.Color.Orange;
+            this.btnStar.Location = new System.Drawing.Point(242, 319);
+            this.btnStar.Name = "btnStar";
+            this.btnStar.Size = new System.Drawing.Size(71, 23);
+            this.btnStar.TabIndex = 1;
+            this.btnStar.Text = "start";
+            this.btnStar.UseVisualStyleBackColor = true;
+            this.btnStar.Click += new System.EventHandler(this.btnStar_Click);
+            // 
+            // lbPoints
+            // 
+            this.lbPoints.AutoSize = true;
+            this.lbPoints.ForeColor = System.Drawing.Color.DarkOrange;
+            this.lbPoints.Location = new System.Drawing.Point(17, 323);
+            this.lbPoints.Name = "lbPoints";
+            this.lbPoints.Size = new System.Drawing.Size(13, 13);
+            this.lbPoints.TabIndex = 2;
+            this.lbPoints.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 361);
+            this.Controls.Add(this.lbPoints);
+            this.Controls.Add(this.btnStar);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.FloralWhite;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Snake";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnStar;
+        private System.Windows.Forms.Label lbPoints;
+    }
+}
+
---
+++ Snake/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        Game oGame;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStar_Click(object sender, EventArgs e)
+        {
+            oGame = new Game(pictureBox1,lbPoints);
+
+            timer1.Enabled= true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!oGame.IsLost)
+            {
+                oGame.Next();
+                oGame.Show();
+            }
+            else
+            {
+                timer1.Enabled= false;
+                MessageBox.Show("Has Perdido");
+
+            } 
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W || e.KeyCode == Keys.NumPad8) oGame.ActualDirection = Game.Direction.Up;
+            if (e.KeyCode == Keys.D || e.KeyCode == Keys.NumPad6) oGame.ActualDirection = Game.Direction.Right;
+            if (e.KeyCode == Keys.A || e.KeyCode == Keys.NumPad4) oGame.ActualDirection = Game.Direction.Left;
+            if (e.KeyCode == Keys.S || e.KeyCode == Keys.NumPad2) oGame.ActualDirection = Game.Direction.Down;
+        }
+    }
+}
